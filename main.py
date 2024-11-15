15b7a04ce3f03801b5479e133cd1b946523aa306
aleksanderpytel <a.pytel@ostfalia.de>
2024-11-15 20:03:20 +0100

    Scroollable frame ist da

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import customtkinter
 
-class RaumFrame(customtkinter.CTkFrame):
-    def __init__(self, master, title):
+class RaumFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, title, values):
         super().__init__(master)
         self.grid_columnconfigure(0, weight=1)
+        self.values = values
+
+        self.raeume = []
         self.title_label = customtkinter.CTkLabel(self, text=title, fg_color="gray30", corner_radius=6)
         self.title_label.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
 
-        self.raum_1 = customtkinter.CTkButton(self, text = "A068", command=self.raum_geklickt)
-        self.raum_1.grid(row=1, column=0, padx=10, pady=(10,0), sticky="w")
-        self.raum_2 = customtkinter.CTkButton(self, text="A069", command=self.raum_geklickt)
-        self.raum_2.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+        for i, value in enumerate(self.values):
+            raum = customtkinter.CTkButton(self, text = value, command=self.raum_geklickt)
+            raum.grid(row = i + 1, column=0, padx=10, pady=(10,0), sticky="w")
+            self.raeume.append(raum)
+
     def raum_geklickt(self):
         print("Raum ausgewählt")
 class Hauptansicht(customtkinter.CTk):
@@ -20,7 +24,8 @@ class Hauptansicht(customtkinter.CTk):
         self.geometry("700x1080")
         self.grid_columnconfigure((0, 1, 2), weight=1)
         self.grid_rowconfigure((0, 1, 2), weight = 1)
-        self.raum_frame = RaumFrame(self, "Räume")
+        values = ["A068", "A069","A068", "A069","A068", "A069","A068", "A069" ]
+        self.raum_frame = RaumFrame(self, "Räume", values = values)
         self.raum_frame.grid(row=1, column=0,padx=10, pady=(10, 0), sticky="w", columnspan = 2)
         self.button =customtkinter.CTkButton(self, text="Notsteuerung", fg_color="red", command=self.button_callback)
         self.button.grid(row=0, column=0, padx=20, pady=20,sticky="ew", columnspan=1)
